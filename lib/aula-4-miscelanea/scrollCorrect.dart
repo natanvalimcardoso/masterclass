c0edad4cee807f1467bae9b55bff0271c94396be
natanvalimcardoso <natanvalimcardoso@rede.ulbra.br>
2022-02-13 17:26:13 -0300

    listview

--- lib/aula-4-miscelanea/scrollCorrect.dart
+++ lib/aula-4-miscelanea/scrollCorrect.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class ScrollCorrect extends StatelessWidget {
@@ -6,24 +8,51 @@ class ScrollCorrect extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: ListView(
-        children: [
-          Container(
-            width: 300,
-            height: 300,
-            color: Colors.blue,
-          ),
-          Container(
-            width: 300,
-            height: 300,
-            color: Color.fromARGB(255, 156, 107, 34),
-          ),
-          Container(
-            width: 300,
-            height: 300,
-            color: Color.fromARGB(255, 163, 52, 0),
-          ),
-        ],
+      child: SizedBox(
+        child: ListView(
+          children: [
+            Container(
+              width: 300,
+              height: 300,
+              color: Colors.blue,
+            ),
+            Align(
+              child: Container(
+                width: 200,
+                height: 300,
+                color: Color.fromARGB(255, 156, 156, 34),
+              ),
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                  margin: EdgeInsets.only(right: 10),
+                  width: 150,
+                  color: Color.fromARGB(255, 255, 9, 9),
+                ),
+                Container(
+                  margin: EdgeInsets.only(right: 10),
+                  width: 150,
+                  color: Color.fromARGB(255, 104, 33, 33),
+                ),
+                Container(
+                  margin: EdgeInsets.only(right: 10),
+                  width: 150,
+                  color: Color.fromARGB(255, 9, 255, 9),
+                ),
+                Container(
+                  margin: EdgeInsets.only(right: 10),
+                  width: 150,
+                  color: Color.fromARGB(255, 21, 66, 133),
+                ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
